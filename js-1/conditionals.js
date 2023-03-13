99f8022f4868bc40aecc9365337dba117697a020
David Sarria <david.sarriav@gmail.com>
2023-03-13 10:26:50 +0100

    Solucion a los ejercicios de js - segundo commit

--- js-1/conditionals.js
+++ js-1/conditionals.js
@@ -16,7 +16,7 @@ const calcularColor = (num) => {
 }
 
 // código de prueba
-console.log(calcularColor(1)) // "El color es negro"
-console.log(calcularColor(2)) // "El color es blanco"
-console.log(calcularColor(3)) // "El color es azul"
-console.log(calcularColor(8)) // "El color es verde"
+console.log(calcularColor(1)); // "El color es negro"
+console.log(calcularColor(2)); // "El color es blanco"
+console.log(calcularColor(3)); // "El color es azul"
+console.log(calcularColor(8)); // "El color es verde"
